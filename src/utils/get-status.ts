736af41b4af11cdd0f4544e4ba1e30385f09a9ee
Eugene Lanets <swarrt@yandex.ru>
2023-07-06 22:37:40 +0300

    feat: add guess letters statuses

--- src/utils/get-status.ts
+++ src/utils/get-status.ts
@@ -1,17 +1,35 @@
 import { type ICell, Status, type StatusCell } from '../types/rows';
 
-const getStatus = (guess: ICell[], answer: string[]): StatusCell[] => {
+const getStatus = (guess: ICell[], answer: string): StatusCell[] => {
+  const answerSet = new Set(answer);
+  const presents: Record<string, number> = {};
   const result = guess.map((cell, idx) => {
-    const status = cell.letter === answer[idx] ? Status.Correct : Status.Absent;
+    let status: Status;
+    if (answerSet.has(cell.letter)) {
+      presents[cell.letter] ??= idx;
+      status = Status.Present;
+    }
+
+    if (cell.letter === answer[idx]) {
+      status = Status.Correct;
+      presents[cell.letter] = idx;
+    }
+
+    status ??= Status.Absent;
+
     return {
       ...cell,
       status,
     };
   });
 
-  console.log(result);
-
-  return result;
+  return result.map((cell, idx) => {
+    if (cell.status !== Status.Present) return cell;
+    return {
+      ...cell,
+      status: presents[cell.letter] === idx ? Status.Present : Status.Absent,
+    };
+  });
 };
 
 export default getStatus;
